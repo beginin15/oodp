adc01b93d55cf54f2f5e6497333f5ef87cf26fba
Jay <beginin15@gmail.com>
2020-10-27 23:30:39 +0900

    [#1] refactor: 메뉴에 대한 공통된 행위를 인터페이스로 정의

--- src/main/java/madvirus/oodp/chap01/Application.java
+++ src/main/java/madvirus/oodp/chap01/Application.java
@@ -5,78 +5,28 @@ public class Application implements OnClickListener {
     private Menu menu1 = new Menu("menu1");
     private Menu menu2 = new Menu("menu2");
     private Button button1 = new Button("button1");
-    private Button button2 = new Button("button2");
-    private Button button3 = new Button("button3");
 
-    private String currentMenu = null;
+    private ScreenUI currentScreen = null;
 
     public Application() {
         menu1.setOnClickListener(this);
         menu2.setOnClickListener(this);
         button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
-        button3.setOnClickListener(this);
     }
 
     @Override
     public void clicked(Component eventSource) {
         if (eventSource.getId().equals("menu1")) {
-            changeUIToMenu1();
+            currentScreen = new Menu1ScreenUI();
+            currentScreen.show();
         } else if (eventSource.getId().equals("menu2")) {
-            changeUIToMenu2();
+            currentScreen = new Menu2ScreenUI();
+            currentScreen.show();
         } else if (eventSource.getId().equals("button1")) {
-            if (currentMenu == null)
+            if (currentScreen == null)
                 return;
-            if (currentMenu.equals("menu1"))
-                processButton1WhenMenu1();
-            else if (currentMenu.equals("menu2"))
-                processButton1WhenMenu2();
-        } else if (eventSource.getId().equals("button2")) {
-            if (currentMenu == null)
-                return;
-            if (currentMenu.equals("menu1"))
-                processButton2WhenMenu1();
-            else if (currentMenu.equals("menu2"))
-                processButton2WhenMenu2();
-        } else if (eventSource.getId().equals("button3")) {
-            if (currentMenu == null)
-                return;
-            if (currentMenu.equals("menu1"))
-                processButton3WhenMenu1();
-            else if (currentMenu.equals("menu2"))
-                processButton3WhenMenu2();
+            // 현재 메뉴에 상관없이 버튼1에 대해 처리한다.
+            currentScreen.handleButton1Click();
         }
-    }
-
-    private void changeUIToMenu1() {
-        currentMenu = "menu1";
-        System.out.println("메뉴1 화면으로 전환");
-    }
-
-    private void changeUIToMenu2() {
-        currentMenu = "menu2";
-        System.out.println("메뉴2 화면으로 전환");
-    }
-
-    private void processButton1WhenMenu1() {
-        System.out.println("메뉴1 화면의 버튼1 처리");
-    }
-    private void processButton1WhenMenu2() {
-        System.out.println("메뉴2 화면의 버튼1 처리");
-    }
-    private void processButton2WhenMenu1() {
-        System.out.println("메뉴1 화면의 버튼2 처리");
-    }
-
-    private void processButton2WhenMenu2() {
-        System.out.println("메뉴2 화면의 버튼2 처리");
-    }
-
-    private void processButton3WhenMenu1() {
-        System.out.println("메뉴1 화면의 버튼3 처리");
-    }
-
-    private void processButton3WhenMenu2() {
-        System.out.println("메뉴2 화면의 버튼3 처리");
     }
 }
---
+++ src/main/java/madvirus/oodp/chap01/ScreenUI.java
@@ -0,0 +1,6 @@
+package madvirus.oodp.chap01;
+
+public interface ScreenUI {
+    public void show();
+    public void handleButton1Click();
+}
